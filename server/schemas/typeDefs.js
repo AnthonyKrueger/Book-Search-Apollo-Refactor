56800e876c03f7806e7a796be040dd8a0cea0bae
aik <anthonykrueger0@gmail.com>
2021-09-05 20:28:29 -0500

    basic gql routes made

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,25 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+    type Book {
+        authors: [String]
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        savedBooks: [Book]
+    }
+
+    type Query {
+        getSingleUser(userId: ID!): User
+    }
+`
